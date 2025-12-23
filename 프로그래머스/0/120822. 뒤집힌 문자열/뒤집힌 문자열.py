3eba76f70d967e91b4945abc58bda89756e04157
DK <wwww0127@naver.com>
2025-12-23 22:31:35 +0900

    [level 0] Title: 뒤집힌 문자열, Time: 0.00 ms, Memory: 9.09 MB -BaekjoonHub

--- 프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.py
+++ 프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.py
@@ -1,5 +1,7 @@
 def solution(my_string):
-    result = ''
-    for x in my_string[::-1]:
-        result += x
+    # result = ''
+    # for x in my_string[::-1]:
+    #     result += x
+    
+    result = my_string[::-1]
     return result
