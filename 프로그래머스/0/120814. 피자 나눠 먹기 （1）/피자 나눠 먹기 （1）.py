7104cde3818049ce5ed9bfbd4e6ddd95eb43b3db
DK <wwww0127@naver.com>
2025-12-24 12:10:33 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.01 ms, Memory: 9.14 MB -BaekjoonHub

--- 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.py
+++ 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.py
@@ -1,8 +1,15 @@
 def solution(n):
-    p = 7
+#     p = 7
 
-    if n % p == 0:
-        result = n // p
+#     if n % p == 0:
+#         result = n // p
+#     else:
+#         result = (n // p) + 1
+    
+    result = 0
+    
+    if n % 7 == 0:
+        result = n // 7
     else:
-        result = (n // p) + 1
+        result = (n // 7) + 1
     return result
