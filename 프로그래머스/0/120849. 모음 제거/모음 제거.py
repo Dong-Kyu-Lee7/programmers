4c9fc026be3a526a450fe335c85bcf106791d766
DK <wwww0127@naver.com>
2025-12-30 13:12:32 +0900

    [level 0] Title: 모음 제거, Time: 0.00 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.py
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.py
@@ -1,6 +1,12 @@
 def solution(my_string):
+    # result = ''
+    # for x in my_string:
+    #     if not x in 'aeiou':
+    #         result += x
+    cond = 'aeiou'
     result = ''
+    
     for x in my_string:
-        if not x in 'aeiou':
+        if x not in cond:
             result += x
     return result
