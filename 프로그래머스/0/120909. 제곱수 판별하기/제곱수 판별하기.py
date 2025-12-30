1de7f4fa420a34b82da778a51a2aceba5ff28796
DK <wwww0127@naver.com>
2025-12-30 13:03:22 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.67 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.py
+++ 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.py
@@ -1,6 +1,10 @@
 def solution(n):
-    if n ** (1/2) != int(n ** (1/2)):
-        result = 2
-    else:
+    # if n ** (1/2) != int(n ** (1/2)):
+    #     result = 2
+    # else:
+    #     result = 1
+    if n ** (1/2) == int(n ** (1/2)):
         result = 1
+    else:
+        result = 2
     return result
