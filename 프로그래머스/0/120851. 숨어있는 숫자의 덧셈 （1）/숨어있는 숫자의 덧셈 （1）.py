9f2437df5b10b90624f23b28850ace2724e61b45
DK <wwww0127@naver.com>
2026-01-01 23:27:31 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.03 ms, Memory: 9.42 MB -BaekjoonHub

--- 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.py
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.py
@@ -1,9 +1,15 @@
 import string
 
 def solution(my_string):
-    low_str = string.ascii_letters
+    # low_str = string.ascii_letters
+    # result = 0
+    # for x in my_string:
+    #     if not x in low_str:
+    #         result +=int(x)
+            
+    cond = string.ascii_letters
     result = 0
     for x in my_string:
-        if not x in low_str:
-            result +=int(x)
+        if x not in cond:
+            result += int(x)
     return result
