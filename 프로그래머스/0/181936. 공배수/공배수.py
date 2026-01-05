d33144076ea6893786f1cd4ae25cc1d49be4f796
DK <wwww0127@naver.com>
2026-01-05 14:49:45 +0900

    [level 0] Title: 공배수, Time: 0.00 ms, Memory: 9.08 MB -BaekjoonHub

--- 프로그래머스/0/181936. 공배수/공배수.py
+++ 프로그래머스/0/181936. 공배수/공배수.py
@@ -1,3 +1,7 @@
 def solution(number, n, m):
-    answer = int((number % n == 0) & (number % m == 0))
-    return answer
+    # answer = int((number % n == 0) & (number % m == 0))
+    
+    result = 0
+    if (number % n == 0) & (number % m == 0):
+        result = 1
+    return result
