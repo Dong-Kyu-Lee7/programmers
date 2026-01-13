a8024b357b35ca714196fdb1aff9516cf503167f
DK <wwww0127@naver.com>
2026-01-13 13:07:04 +0900

    [level 0] Title: n의 배수, Time: 0.01 ms, Memory: 9.22 MB -BaekjoonHub

--- 프로그래머스/0/181937. n의 배수/n의 배수.py
+++ 프로그래머스/0/181937. n의 배수/n의 배수.py
@@ -1,6 +1,7 @@
 def solution(num, n):
+    result = 0
+    
     if num % n == 0:
-        answer = 1
-    else:
-        answer = 0
-    return answer
+        result = 1
+        
+    return result
