482fda026aa8fb7934e58202f5fdbeed6108e71b
DK <wwww0127@naver.com>
2026-01-14 12:04:54 +0900

    [level 0] Title: 수 조작하기 1, Time: 8.91 ms, Memory: 9.44 MB -BaekjoonHub

--- 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.py
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.py
@@ -1,15 +1,24 @@
 def solution(n, control):
-    answer = n
+#     answer = n
     
-    for i in control:
-        if i == 'w':
-            answer += 1
+#     for i in control:
+#         if i == 'w':
+#             answer += 1
             
-        elif i == 's':
-            answer -= 1
+#         elif i == 's':
+#             answer -= 1
             
-        elif i == 'd':
-            answer += 10
-        else:
-            answer -= 10
-    return answer
+#         elif i == 'd':
+#             answer += 10
+#         else:
+#             answer -= 10
+    for x in control:
+        if x == 'w':
+            n += 1
+        elif x == 's':
+            n -= 1
+        elif x == 'd':
+            n += 10
+        elif x == 'a':
+            n -= 10
+    return n
