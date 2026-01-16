0cc307cf8417fb7100ec670fc3d7d86adf5d44e8
DK <wwww0127@naver.com>
2026-01-16 12:39:40 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.02 ms, Memory: 9.32 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.py
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.py
@@ -1,9 +1,15 @@
 def solution(a, b):
-    cond1 = str(a) + str(b)
-    cond2 = str(b) + str(a)
+#     cond1 = str(a) + str(b)
+#     cond2 = str(b) + str(a)
     
-    if int(cond1) > int(cond2):
-        answer = int(cond1)
-    else:
-        answer = int(cond2)
-    return answer
+#     if int(cond1) > int(cond2):
+#         answer = int(cond1)
+#     else:
+#         answer = int(cond2)
+    cond1 = int(str(a) + str(b))
+    cond2 = int(str(b) + str(a))
+
+    result = cond2
+    if cond1 > cond2:
+        result = cond1
+    return result
