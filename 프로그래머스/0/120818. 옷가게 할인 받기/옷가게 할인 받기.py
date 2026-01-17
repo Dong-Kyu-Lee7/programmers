bfa8ec09f799b9626fe6054e85fde6b9f97122d0
DK <wwww0127@naver.com>
2026-01-17 12:47:05 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.00 ms, Memory: 9.24 MB -BaekjoonHub

--- 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
@@ -1,11 +1,24 @@
 def solution(price):
-    # result = 0
-    if price >= 100000 and price < 300000:
+    ## result = 0
+    # if price >= 100000 and price < 300000:
+    #     result = 0.95 * price
+    # elif price >= 300000 and price < 500000:
+    #     result = 0.9 * price
+    # elif price >= 500000:
+    #     result = 0.8 * price
+    # elif price < 100000:
+    #     result = price
+    
+    cond1 = 100000
+    cond2 = 300000
+    cond3 = 500000
+    
+    if price >= cond1 and price < cond2:
         result = 0.95 * price
-    elif price >= 300000 and price < 500000:
+    elif price >= cond2 and price < cond3:
         result = 0.9 * price
-    elif price >= 500000:
+    elif price >= cond3:
         result = 0.8 * price
-    elif price < 100000:
+    else:
         result = price
     return int(result)
