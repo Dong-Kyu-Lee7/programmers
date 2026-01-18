f55cdfbc7ffd1340ea6f796ef43bbbae2e51b762
DK <wwww0127@naver.com>
2026-01-18 16:57:11 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.03 ms, Memory: 9.54 MB -BaekjoonHub

--- 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.py
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.py
@@ -3,8 +3,11 @@ import string
 def solution(my_string):
     low_str = string.ascii_lowercase
     result = []
+    # for x in my_string:
+    #     if not x in low_str:
+    #         result.append(int(x))
     for x in my_string:
-        if not x in low_str:
+        if x not in low_str:
             result.append(int(x))
     result = sorted(result)
     return result
