c113ebb3e6f625c4b14685e4532054a0f4bd6548
DK <wwww0127@naver.com>
2026-01-20 12:41:40 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 0.03 ms, Memory: 9.11 MB -BaekjoonHub

--- 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.py
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.py
@@ -1,10 +1,18 @@
 def solution(n):
     num_lst = [n]
 
-    while n != 1:
+    # while n != 1:
+    #     if n % 2 == 0:
+    #         n = int(n/2)
+    #     else:
+    #         n = int(n*3+1)
+    #     num_lst.append(n)
+    for _ in range(1000):
+        if n == 1:
+            break
         if n % 2 == 0:
-            n = int(n/2)
+            n = n // 2
         else:
-            n = int(n*3+1)
+            n = n * 3 +1
         num_lst.append(n)
     return num_lst
