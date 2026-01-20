20448061c143c7aa619d7fbda8260ff741675e35
DK <wwww0127@naver.com>
2026-01-20 11:58:39 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.12 ms, Memory: 9.05 MB -BaekjoonHub

--- 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.py
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.py
@@ -1,10 +1,10 @@
 def solution(my_string, is_suffix):
     check_list = []
-
+    
+    result = 0
     for x in range(len(my_string)):
         check_list.append(my_string[x:])
         if is_suffix in check_list:
             result = 1
-        else:
-            result = 0
+
     return result
