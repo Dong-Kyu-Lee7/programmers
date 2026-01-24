08540206104bcb470f95f7b60ab062dbff9d557b
DK <wwww0127@naver.com>
2026-01-24 16:06:55 +0900

    [level 0] Title: 숫자 찾기, Time: 0.01 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.py
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.py
@@ -1,7 +1,8 @@
 def solution(num, k):
     result = -1
+    
     for idx,val in enumerate(str(num)):
         if val == str(k):
-            result = idx+1
+            result = idx + 1
             break
     return result
