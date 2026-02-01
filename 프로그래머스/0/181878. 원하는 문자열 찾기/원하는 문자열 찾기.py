c3eb6b4368da6b8bf8546fed5f0c99916b08f6f3
DK <wwww0127@naver.com>
2026-02-01 15:33:47 +0900

    [level 0] Title: 원하는 문자열 찾기, Time: 0.17 ms, Memory: 9.32 MB -BaekjoonHub

--- 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.py
+++ 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.py
@@ -2,6 +2,6 @@ def solution(myString, pat):
     result = 0
     if pat.lower() in myString.lower():
         result = 1
-    else:
-        result = 0
+    # else:
+    #     result = 0
     return result
