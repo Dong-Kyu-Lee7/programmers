b56da2511cd99733f75560e057a346d081cd8851
DK <wwww0127@naver.com>
2026-02-06 14:45:48 +0900

    [level 0] Title: 수 조작하기 2, Time: 27.20 ms, Memory: 13.2 MB -BaekjoonHub

--- 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.py
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.py
@@ -2,13 +2,15 @@ def solution(numLog):
     result = ''
     
     
-    for i in range(1,len(numLog)):
+    for i in range(1,len(numLog)): # 0부터 실행시 'numLog[i-1]' 계산 불가
         if numLog[i] - numLog[i-1] == 1:
             result += 'w'
         elif numLog[i] - numLog[i-1] == -1:
             result += 's'
         elif numLog[i] - numLog[i-1] == 10:
             result += 'd'
-        else:
+        elif numLog[i] - numLog[i-1] == -10:
             result += 'a'
+        # else:
+        #     result += 'a'
     return result
