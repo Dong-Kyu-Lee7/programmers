ba57f10d3cb9f0bde74bf9bc1864ebe004090a3b
DK <wwww0127@naver.com>
2026-02-08 15:08:57 +0900

    [level 0] Title: 문자열 섞기, Time: 0.01 ms, Memory: 9.31 MB -BaekjoonHub

--- 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.py
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.py
@@ -1,6 +1,11 @@
 def solution(str1, str2):
-    answer = ""
-    for i in range(0, len(str1)):
-        answer = answer + str1[i] + str2[i]
-        
-    return answer
+#     answer = ""
+#     for i in range(0, len(str1)):
+#         answer = answer + str1[i] + str2[i]
+    
+    result = ''
+
+    for x in range(len(str1)):
+        result += str1[x] + str2[x]
+
+    return result
