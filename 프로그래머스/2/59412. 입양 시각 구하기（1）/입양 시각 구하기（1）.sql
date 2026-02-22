46f553e835fc3e1bba05581a98900bc7a5932be8
DK <wwww0127@naver.com>
2026-02-22 12:12:00 +0900

    [level 2] Title: 입양 시각 구하기(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59412. 입양 시각 구하기（1）/입양 시각 구하기（1）.sql
+++ 프로그래머스/2/59412. 입양 시각 구하기（1）/입양 시각 구하기（1）.sql
@@ -4,4 +4,4 @@ select hour(DATETIME) HOUR, count(ANIMAL_ID) count
 from ANIMAL_OUTS
 where hour(DATETIME) >= 9 and hour(DATETIME) <= 19
 group by hour(DATETIME)
-order by hour(DATETIME)
+order by hour(DATETIME) asc
