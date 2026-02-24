df08f1272576b4757849e82ca8da2d53328161c3
DK <wwww0127@naver.com>
2026-02-24 12:48:33 +0900

    [level 3] Title: 카테고리 별 도서 판매량 집계하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/144855. 카테고리 별 도서 판매량 집계하기/카테고리 별 도서 판매량 집계하기.sql
+++ 프로그래머스/3/144855. 카테고리 별 도서 판매량 집계하기/카테고리 별 도서 판매량 집계하기.sql
@@ -6,6 +6,4 @@ join book_sales bs
 on b.book_id = bs.book_id
 where year(bs.sales_date) = 2022 and month(bs.sales_date) = 1
 group by b.category
-# having substr(bs.sales_date, 1,7) = '2022-01'
-
-order by category
+order by category asc
