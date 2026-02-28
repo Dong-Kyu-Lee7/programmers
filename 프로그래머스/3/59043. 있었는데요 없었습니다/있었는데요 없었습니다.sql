c0a0d9ef64d83b35c1fbce50b8b1763749ffbe1f
DK <wwww0127@naver.com>
2026-02-28 13:20:29 +0900

    [level 3] Title: 있었는데요 없었습니다, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
+++ 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
@@ -3,7 +3,7 @@
 select i.animal_id, i.name
 from ANIMAL_INS i
 join ANIMAL_OUTS o
-on i.animal_id = o.animal_id
+on i.animal_id = o.animal_id # 오류로 입양날짜가 더 미래된 날짜 필터
 where i.datetime > o.datetime
 # group by i.ANIMAL_ID
-order by i.datetime
+order by i.datetime asc
