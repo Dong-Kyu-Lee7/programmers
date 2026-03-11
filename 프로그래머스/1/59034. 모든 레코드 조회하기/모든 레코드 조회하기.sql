f98932f3f07da1bbd05330fad73e60af468e3669
DK <wwww0127@naver.com>
2026-03-11 14:33:12 +0900

    [level 1] Title: 모든 레코드 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/59034. 모든 레코드 조회하기/모든 레코드 조회하기.sql
+++ 프로그래머스/1/59034. 모든 레코드 조회하기/모든 레코드 조회하기.sql
@@ -1,4 +1,5 @@
 -- 코드를 입력하세요
-SELECT *
-from ANIMAL_INS 
-order by ANIMAL_ID
+# SELECT *
+select ANIMAL_ID, ANIMAL_TYPE, DATETIME, INTAKE_CONDITION, name, SEX_UPON_INTAKE
+from animal_ins
+order by animal_id asc
