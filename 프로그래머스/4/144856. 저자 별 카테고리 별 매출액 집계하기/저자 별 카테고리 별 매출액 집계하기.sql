7cff7b10dc546e2af3801237ee7c29586f4128b6
DK <wwww0127@naver.com>
2026-03-18 01:08:44 +0900

    [level 4] Title: 저자 별 카테고리 별 매출액 집계하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/144856. 저자 별 카테고리 별 매출액 집계하기/저자 별 카테고리 별 매출액 집계하기.sql
+++ 프로그래머스/4/144856. 저자 별 카테고리 별 매출액 집계하기/저자 별 카테고리 별 매출액 집계하기.sql
@@ -5,7 +5,8 @@ join AUTHOR a
 on b.AUTHOR_ID = a.AUTHOR_ID
 join BOOK_SALES bs
 on b.BOOK_ID = bs.BOOK_ID
-where bs.sales_date like '2022-01%'
+# where bs.sales_date like '2022-01%'
+where bs.sales_date >= '2022-01-01' and bs.sales_date < '2022-02-01'
 group by a.AUTHOR_ID, b.CATEGORY
 # having sum(bs.sales) * b.price
 order by a.AUTHOR_ID, b.CATEGORY desc
