953f69c7937e66ea36d77dfe434d5378ca8c6a06
DK <wwww0127@naver.com>
2026-03-26 00:36:46 +0900

    [level 1] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
+++ 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -9,5 +9,6 @@ SELECT
 from USED_GOODS_BOARD u1
 join USED_GOODS_REPLY u2
 on u1.board_id = u2.BOARD_ID
-where u1.CREATED_DATE like '2022-10-%'
+# where u1.CREATED_DATE like '2022-10-%'
+where u1.created_date >= '2022-10-01' and u1.created_date < '2022-11-01'
 order by u2.CREATED_DATE, u1.TITLE
