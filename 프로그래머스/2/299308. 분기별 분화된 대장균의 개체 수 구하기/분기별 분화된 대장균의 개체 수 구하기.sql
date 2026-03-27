8f85420db49e53d821a93b1aa2432eeb63c6f4df
DK <wwww0127@naver.com>
2026-03-27 14:39:16 +0900

    [level 2] Title: 분기별 분화된 대장균의 개체 수 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/299308. 분기별 분화된 대장균의 개체 수 구하기/분기별 분화된 대장균의 개체 수 구하기.sql
+++ 프로그래머스/2/299308. 분기별 분화된 대장균의 개체 수 구하기/분기별 분화된 대장균의 개체 수 구하기.sql
@@ -1,12 +1,30 @@
 -- 코드를 작성해주세요
-select 
-    case
-        when month(DIFFERENTIATION_DATE) <=3 then '1Q'
-        when month(DIFFERENTIATION_DATE) <=6 then '2Q'
-        when month(DIFFERENTIATION_DATE) <=9 then '3Q'
-        else '4Q'
-    end as QUARTER,
-    count(id) ECOLI_COUNT
-from ECOLI_DATA 
-group by QUARTER
-order by QUARTER
+# select 
+#     case
+#         when month(DIFFERENTIATION_DATE) <= 3 then '1Q'
+#         when month(DIFFERENTIATION_DATE) <= 6 then '2Q'
+#         when month(DIFFERENTIATION_DATE) <= 9 then '3Q'
+#         else '4Q'
+#     end as QUARTER,
+#     count(id) ECOLI_COUNT
+# from ECOLI_DATA 
+# group by QUARTER
+# order by QUARTER
+
+SELECT 
+    CASE
+        WHEN MONTH(DIFFERENTIATION_DATE) <= 3 THEN '1Q'
+        WHEN MONTH(DIFFERENTIATION_DATE) <= 6 THEN '2Q'
+        WHEN MONTH(DIFFERENTIATION_DATE) <= 9 THEN '3Q'
+        ELSE '4Q'
+    END AS QUARTER,
+    COUNT(ID) AS ECOLI_COUNT
+FROM ECOLI_DATA
+GROUP BY 
+    CASE
+        WHEN MONTH(DIFFERENTIATION_DATE) <= 3 THEN '1Q'
+        WHEN MONTH(DIFFERENTIATION_DATE) <= 6 THEN '2Q'
+        WHEN MONTH(DIFFERENTIATION_DATE) <= 9 THEN '3Q'
+        ELSE '4Q'
+    END
+ORDER BY QUARTER;
